5af4646c3666786617dbb64d58bc364eba69cef0
Barth <barth@paleologue.fr>
2018-08-18 12:11:17 +0200

    quick fixes

--- js/actions.js
+++ js/actions.js
@@ -15,4 +15,22 @@ const actions = [
     "distribue des goulags",
     "dénonce son voisin",
     "essaye de comprendre son propre code",
+    "consulte votre dossier",
 ]
+
+/*  [AhjcjNzzjdOjzbMzjbdAejeLxjiY]
+
+    - Oui et donc Michel, une fois que j'ai les codes d'accès j'ai plus qu'à...
+
+    - Tais-toi Michel ! J'ai l'impression que quelqu'un nous écoute !
+
+    - Mais enfin Michel, tu sais bien qu'il n'y a que nous ici, qui s'embèterait vraiment au point de venir ici ?
+
+    - Je sais bien Michel, mais mon intuition me trompe rarement.
+
+    - Dans ce cas t'as qu'à lui demander de partir, Michel.
+
+    - Oui je vais faire ça (se tourne vers toi) Et toi là ! Qu'est-ce que tu fais là ?
+      Tu nous espionnes ? Retourne chez toi avant qu'on récupère ton IP !
+
+*/
