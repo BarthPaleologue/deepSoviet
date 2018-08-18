b22712280bae8378fb4d073e410c1b53bf7a1f16
Barth <barth@paleologue.fr>
2018-08-18 11:34:46 +0200

    Initial commit

---
+++ js/actions.js
@@ -0,0 +1,18 @@
+Array.prototype.getRandomIndex = function() {
+    return this[Math.floor(Math.random() * this.length)]
+}
+const actions = [
+    "envahit la Pologne",
+    "répends l'idéologie",
+    "met en commun les moyens de production",
+    "construit un goulag",
+    "organise la propagande",
+    "élimine les opposants",
+    "lit la Pravda",
+    "organise un congrès du parti",
+    "conseille Vladimir Poutine",
+    "rejoue la bataille de Stalingrad",
+    "distribue des goulags",
+    "dénonce son voisin",
+    "essaye de comprendre son propre code",
+]
